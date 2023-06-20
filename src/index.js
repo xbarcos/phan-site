00b8d0d98ea7b4873650e9fbda17aaf695d127fb
João Marcos <jmarcos@joywork.com.br>
2023-06-20 17:58:19 -0300

    background_change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"></link>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
